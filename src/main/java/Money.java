aef17988a87db36875aae7bd895882620db02b96
Timo <developer.timo@gmail.com>
2022-07-24 10:02:26 +0900

    refactor: Money에 Dollar를 반환하는 팩토리 메서드 추가
    - 하위 클래스에 대한 직접적인 참조를 줄이기 위한 작업

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -2,6 +2,16 @@ public class Money {
 
     protected int amount;
 
+    /**
+     * Dollar를 반환하는 팩토리 메서드
+     *
+     * @param amount
+     * @return Dollar 인스턴스
+     */
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
