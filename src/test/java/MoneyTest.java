548417777de01db9292aad7a547a58d9503f3a32
Timo <developer.timo@gmail.com>
2022-07-24 10:29:30 +0900

    9장. 우리가 사는 시간
    - 통화 개념 도입을 위한 테스트 추가

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -36,4 +36,13 @@ public class MoneyTest {
         assertEquals(Money.franc(15), five.times(3));
     }
 
+    /**
+     * 통화(currency) 개념
+     */
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
+
 }
