58095ba6f129262f4ca63efae6e48f1379c80987
Timo <developer.timo@gmail.com>
2022-07-24 10:31:20 +0900

    fix: currency() 메서드 선언, 구현을 통해 컴파일 에러 해결

--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -8,4 +8,9 @@ public class Franc extends Money {
         return new Franc(amount * multiplier);
     }
 
+    @Override
+    public String currency() {
+        return "CHF";
+    }
+
 }
--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -23,4 +23,5 @@ public abstract class Money {
     }
 
     public abstract Money times(int multiplier);
+    public abstract String currency();
 }
